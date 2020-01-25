bbd3f896c3aee7490aab45a91faa59375123382d
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-01-25 20:51:40 +0100

    Fix bug in GOS ATL Led template

--- sketches/testboard/ledtest/ledtest.ino
+++ sketches/testboard/ledtest/ledtest.ino
@@ -11,22 +11,21 @@
 #define PIN_LED_5       5
 #define PIN_LED_6       6
 
-fds::SinLed led_green(PIN_LED_GREEN);
-fds::SinLed led_yellow(PIN_LED_YELLOW);
-fds::SinLed led_blue(PIN_LED_BLUE);
-fds::SinLed led_red(PIN_LED_RED);
-
 float led3 = 0.0F, led4 = 0.0F, led5 = 0.0F, led6 = 0.0F;
 
 namespace gatl = ::gos::atl;
-namespace gatlL = ::gos::atl::led;
-
+namespace gatll = ::gos::atl::led;
 
 void setup() {
-  led_green.initialize();
-  led_yellow.initialize();
-  led_blue.initialize();
-  led_red.initialize();
+  gatll::initialize(PIN_LED_1);
+  gatll::initialize(PIN_LED_2);
+  gatll::initialize(PIN_LED_3);
+  gatll::initialize(PIN_LED_4);
+  gatll::initialize(PIN_LED_5);
+  gatll::initialize(PIN_LED_6);
+
+  gatll::blink<>(PIN_LED_1);
+  gatll::blink<>(PIN_LED_2);
 }
 
 void loop() {
